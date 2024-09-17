6676648485ebbc486495111646147a94c75a9e99
Princess585 <princessdkhombisa1994@gmail.com>
2024-09-17 10:13:36 +0000

    auth

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,19 +7,20 @@ from api.v1.views import app_views
 
 @app_views.route("/status", methods=["GET"], strict_slashes=False)
 def status() -> str:
-    """
-    Return the status jsonify 'OK'
+    """GET /api/v1/status
+    Return:
+      - the status of the API
     """
     return jsonify({"status": "OK"})
 
 
 @app_views.route("/stats/", strict_slashes=False)
 def stats() -> str:
-    """
-    Return jsonify status
+    """GET /api/v1/stats
+    Return:
+      - the number of each objects
     """
     from models.user import User
-
     stats = {}
     stats["users"] = User.count()
     return jsonify(stats)
@@ -27,11 +28,11 @@ def stats() -> str:
 
 @app_views.route("/unauthorized", methods=["GET"], strict_slashes=False)
 def unauthorized():
-    """api is unathorized"""
+    """An unauthorized user"""
     abort(401)
 
 
 @app_views.route("/forbidden", methods=["GET"], strict_slashes=False)
 def forbidden():
-    """The forbidden 403 endpoint"""
+    """The forbidden method"""
     abort(403)
